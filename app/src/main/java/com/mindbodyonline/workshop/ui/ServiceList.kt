54b1f375c4c3d240969cb5d361179ab4e74ac130
Jon F Hancock <jonfhancock@gmail.com>
2021-04-27 12:52:11 -0700

    Set up the BotomSheetScaffold for ServiceList

--- app/src/main/java/com/mindbodyonline/workshop/ui/ServiceList.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/ServiceList.kt
@@ -1,7 +1,15 @@
 package com.mindbodyonline.workshop.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mindbodyonline.workshop.data.model.ServiceId
 import com.mindbodyonline.workshop.ui.model.ServiceCategoryState
 import com.mindbodyonline.workshop.ui.model.ServiceListViewState
@@ -14,7 +22,27 @@ fun ServiceList(
     onCategorySelectionChanged: (ServiceCategoryState) -> Unit,
     navigateToDetail: (ServiceId) -> Unit
 ) {
-
+    BottomSheetScaffold(
+        sheetContent = { Text("Hello Bottom Sheet") },
+        topBar = {
+            TopAppBar(
+                title = { Text(viewState.title) },
+                navigationIcon = {
+                    Icon(
+                        Icons.Default.Spa,
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            )
+        },
+        sheetBackgroundColor = colors.secondaryVariant,
+        sheetPeekHeight = 120.dp,
+        sheetElevation = 4.dp,
+        sheetShape = RoundedCornerShape(4.dp, 4.dp, 0.dp, 0.dp)
+    ) {
+        Text("Hello World")
+    }
 }
 
 @Preview
