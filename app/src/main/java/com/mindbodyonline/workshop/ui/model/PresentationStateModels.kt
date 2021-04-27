0fc62f88b86ad0c5126a12cea5e521612f6679b2
Jon F Hancock <jonfhancock@gmail.com>
2021-04-27 10:17:38 -0700

    Remove commented out code

--- app/src/main/java/com/mindbodyonline/workshop/ui/model/PresentationStateModels.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/model/PresentationStateModels.kt
@@ -10,19 +10,9 @@ val ServiceCategoryState.selected get() = second
 
 sealed class ServiceListViewState(val title: String) {
     object Placeholder : ServiceListViewState("Hello World")
-//    data class Ready(
-//        val services: List<Service>,
-//        val categories: List<ServiceCategoryState>
-//    ) : ServiceListViewState("Salon Services")
 }
 
 
 sealed class ServiceDetailViewState(val title: String) {
     object Placeholder : ServiceDetailViewState("Hello World")
-//    data class Ready(
-//        val service: Service,
-//        val staff: List<Staff>
-//    ) : ServiceDetailViewState(service.name)
-//
-//    object NotFound : ServiceDetailViewState("Not Found")
 }
