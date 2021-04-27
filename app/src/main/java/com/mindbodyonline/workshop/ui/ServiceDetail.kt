795081474162c068d3bab636233970e4fffd1946
Jon F Hancock <jonfhancock@gmail.com>
2021-04-26 22:22:57 -0700

    Add service not found composable with ServiceDetailViewState.

--- app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
@@ -12,12 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.mindbodyonline.workshop.data.SampleData
-import com.mindbodyonline.workshop.data.model.PriceOption
-import com.mindbodyonline.workshop.data.model.Staff
-import com.mindbodyonline.workshop.ui.model.ServiceDetailViewState
-import com.mindbodyonline.workshop.ui.model.toAnnotatedStringList
 import com.mindbodyonline.workshop.ui.theme.MyTheme
 
 @Composable
