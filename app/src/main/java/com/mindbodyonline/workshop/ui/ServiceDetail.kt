d0caee2c88ffbc331870156b2217577f59667645
Jon F Hancock <jonfhancock@gmail.com>
2021-04-27 11:27:25 -0700

    Add baseline support for dark themes in previews

--- app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
@@ -44,8 +44,8 @@ private fun ServiceNotFound() {
 
 @Preview
 @Composable
-fun DetailPreview() {
-    MyTheme {
+fun DetailPreview(darkTheme: Boolean = false) {
+    MyTheme(darkTheme) {
         ServiceDetail(ServiceDetailViewState.NotFound, {})
     }
 }
