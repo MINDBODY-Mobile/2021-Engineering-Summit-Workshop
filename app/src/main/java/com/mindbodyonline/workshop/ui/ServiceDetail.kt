0825814110de1544996effcebb2895204dc79a11
Jon F Hancock <jonfhancock@gmail.com>
2021-04-26 22:51:07 -0700

    Add ServiceDetailViewState.Ready, hero image, and services and staff cards to detail composable

--- app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
@@ -12,6 +12,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mindbodyonline.workshop.data.SampleData
+import com.mindbodyonline.workshop.data.model.PriceOption
+import com.mindbodyonline.workshop.data.model.Service
+import com.mindbodyonline.workshop.data.model.Staff
 import com.mindbodyonline.workshop.ui.theme.MyTheme
 
 @Composable
