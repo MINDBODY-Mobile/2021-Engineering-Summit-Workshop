b79f298a1113f4d649bb8cabc9b510e0ad13495a
Jon F Hancock <jonfhancock@gmail.com>
2021-04-26 22:22:57 -0700

    Add service not found composable with ServiceDetailViewState.

--- app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
+++ app/src/main/java/com/mindbodyonline/workshop/ui/ServiceDetail.kt
@@ -1,13 +1,18 @@
 package com.mindbodyonline.workshop.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mindbodyonline.workshop.ui.model.ServiceDetailViewState
 import com.mindbodyonline.workshop.ui.theme.MyTheme
-
 
 @Composable
 fun ServiceDetail(viewState: ServiceDetailViewState, navigateUp: () -> Unit) {
@@ -18,7 +23,22 @@ fun ServiceDetail(viewState: ServiceDetailViewState, navigateUp: () -> Unit) {
             )
         }
     ) {
-        Text("Hello World!")
+        when (viewState) {
+            ServiceDetailViewState.NotFound -> ServiceNotFound()
+        }
+
+    }
+}
+
+@Composable
+private fun ServiceNotFound() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(colors.surface),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text("We couldn't find that service.")
     }
 }
 
@@ -26,6 +46,6 @@ fun ServiceDetail(viewState: ServiceDetailViewState, navigateUp: () -> Unit) {
 @Composable
 fun DetailPreview() {
     MyTheme {
-        ServiceDetail(ServiceDetailViewState.Placeholder, {})
+        ServiceDetail(ServiceDetailViewState.NotFound, {})
     }
 }
